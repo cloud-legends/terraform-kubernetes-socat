1be266000a5feb533295f7a1e82db8d04afaff19
Glaaj <83582518+Glaaj@users.noreply.github.com>
2021-12-01 10:00:00 +0100

    added variabled inside the module

--- socat.tf
+++ socat.tf
@@ -28,14 +28,11 @@ resource "kubernetes_deployment" "socat_deploy" {
           image   = "alpine/socat"
           name    = "socat-proxy"
           command = ["/bin/sh"]
-          # this way we can retrieve the endpoint from our own RDS module
-          args    = ["-c","socat -d -d TCP-LISTEN:5432,fork TCP:${module.rds.aurora_write_endpoint}:5432"]
-
+          args    = ["-c","socat -d -d ${var.protocol}-LISTEN:${var.source_port},fork ${var.protocol}:${var.endpoint}:${var.destination_port}"]
           resources {
             #I've gone ahead and set some defaults for this, ram usage is already very minimal
             # When passively listening for connections: 1318912 Bytes = 1.3189119999999999 MB
             # When having 1 connection active: 1818624 Bytes = 1.818624 MB
-            
             limits = {
               cpu    = "100m"
               memory = "100mi"
@@ -48,9 +45,8 @@ resource "kubernetes_deployment" "socat_deploy" {
           
           port {
             # This is the port that will be available on the pod to accept a port forward.
-            # We can change this if we want, for now this port works and is easy to remember.
-            container_port = 5432
-            protocol       = "TCP"
+            container_port = var.source_port
+            protocol       = var.protocol
           }  
         }
       }
